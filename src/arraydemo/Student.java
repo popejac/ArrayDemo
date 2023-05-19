a386fffb3242e2a1696060a6d8bcdf511d1b7022
Jacquie Pope <popejac@shernet.sheridancollege.ca>
2023-05-19 10:30:10 -0400

    added address attribute

--- src/arraydemo/Student.java
+++ src/arraydemo/Student.java
@@ -6,10 +6,19 @@ public class Student {
     //comment added at 9:27AM
     private String sId;
     private String sName;
-
+    private String address;
+    
     public Student(String sId, String sName) {
         this.sId = sId;
         this.sName = sName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getsId() {
