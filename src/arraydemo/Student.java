9a1434c51d574199342d0cc1cdc20c7e6f9a1d9a
popejac <134073756+popejac@users.noreply.github.com>
2023-05-26 09:33:01 -0400

    Update Student.java
    added comment for pull

--- src/arraydemo/Student.java
+++ src/arraydemo/Student.java
@@ -6,6 +6,7 @@ public class Student {
     //comment added at 9:27AM
     //comment added for modification
     //comment added for fetch and merge
+    //comment for pull
     private String sId;
     private String sName;
     private String address;
